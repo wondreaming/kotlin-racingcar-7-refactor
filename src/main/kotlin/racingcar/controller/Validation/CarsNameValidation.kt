c6408d472f3eccb62655faec012b7ea84f4eb0bf
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-27 16:52:10 +0900

    feat: 자동차 이름이 5자 이하

--- src/main/kotlin/racingcar/controller/Validation/CarsNameValidation.kt
+++ src/main/kotlin/racingcar/controller/Validation/CarsNameValidation.kt
@@ -6,6 +6,7 @@ class CarsNameValidation(
     fun validateCarsName() {
         isNotEmpty()
         checkCarsCount()
+        checkCarName5()
     }
 
     private fun isNotEmpty() {
@@ -17,8 +18,14 @@ class CarsNameValidation(
         require(carsCount >= RACING_OK_COUNT) { CarsNameErrorType.UP_1.errorMessage }
     }
 
+    private fun checkCarName5() {
+        val carsName = carsName.split(COMMA)
+        require(carsName.any { it.length <= CAR_NAME_OK_LENGTH }) { CarsNameErrorType.UNDER_5.errorMessage }
+    }
     companion object {
         private const val COMMA = ","
         private const val RACING_OK_COUNT = 2
+        private const val CAR_NAME_OK_LENGTH = 2
+
     }
 }
