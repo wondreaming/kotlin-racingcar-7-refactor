ceba4c0cf3635325cb0f066426825a63eeac6dc5
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-27 21:02:25 +0900

    feat: 랜덤 숫자가 4이상이면 전진

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,8 +1,19 @@
 package racingcar.model
 
+import racingcar.util.RandomNumber
 import javax.swing.text.Position
 
 data class Car(
     private val name: String,
-    private val position: Int = 0,
-)
+    private var _position: Int = 0,
+) {
+    val position: Int
+        get() = _position
+
+    fun move() {
+        val randomNumber = RandomNumber().getRandomNumber()
+        if (randomNumber >= 4) {
+            _position++
+        }
+    }
+}
